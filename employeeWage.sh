8c324fb084f583865c4ac060ccccfadc00512f1a
shravan-kaja <shravan.kaja@gmail.com>
2021-01-19 16:01:10 +0530

    Added: Employee Attandence

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,1 +1,14 @@
 echo "Welcome to Employee Wage Computation System"
+
+function employeeAttandence() {
+rand=$(($RANDOM%2))
+if [ $rand -eq 0 ]
+then 
+	echo "Present"
+elif [ $rand -eq 1 ]
+then
+	echo "Not Present"
+fi
+}
+employeeAttandence
+
